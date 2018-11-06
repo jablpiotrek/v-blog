887fa8d26c597763543f4fac9f643d3dc0628eac
Piotr Jablonski <piotr.jablonski@snow.dog>
2018-11-06 21:56:34 +0100

    Initialize firestore

--- src/store.js
+++ src/store.js
@@ -6,6 +6,11 @@ import "firebase/firestore";
 
 import firebaseConfig from "./firebase-config";
 import firestoreSettings from "./firestore-settings";
+
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
+firestore.settings(firestoreSettings);
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
