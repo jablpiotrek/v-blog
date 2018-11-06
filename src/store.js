2a28197f9afc98df51a7cc052177b3a5c95028eb
Piotr Jablonski <piotr.jablonski@snow.dog>
2018-11-06 21:55:55 +0100

    Import firebase and firestore config files

--- src/store.js
+++ src/store.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+
+import firebaseConfig from "./firebase-config";
+import firestoreSettings from "./firestore-settings";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
