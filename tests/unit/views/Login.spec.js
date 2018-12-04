8a5ba493636565d30861de3f75a01c18689be3e6
Piotr Jablonski <piotr.jablonski@snow.dog>
2018-12-04 18:31:20 +0100

    update unit tests

--- tests/unit/views/Login.spec.js
+++ tests/unit/views/Login.spec.js
@@ -2,12 +2,12 @@ import { expect } from "chai"
 import { mount, createLocalVue } from "@vue/test-utils"
 import Vuex from 'vuex'
 
-import Login from "@/views/Login.vue"
+import Login from '@/views/Login.vue'
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
-describe("Login.vue", () => {
-  let mocks = {
+describe('Login.vue', () => {
+  const mocks = {
     $store: {
       state: {
         currentUser: null
@@ -15,19 +15,38 @@ describe("Login.vue", () => {
     }
   }
 
-  it("Renders without error", () => {
+  it('Renders without error', () => {
     const wrapper = mount(Login, {mocks})
     expect(wrapper.html())
   })
 
-  it("Renders properly for not logged user", () => {
-    const wrapper = mount(Login, {mocks})
-    console.log(wrapper.html())
-    expect(wrapper.html()).to.contain('<p>You are logged in.</p>')
+  describe('Not logged user', () => {
+    const mocks = {
+      $store: {
+        state: {
+          currentUser: null
+        }
+      }
+    }
+
+    it('Renders properly for logged user', () => {
+      const wrapper = mount(Login, {mocks})
+      expect(wrapper.html()).to.contain('name="log-in"')
+    })
   })
-  mocks.$store.state.currentUser = 'not-empty-user-data'
-  it("Renders properly for logged user", () => {
-    const wrapper = mount(Login, {mocks})
-    expect(wrapper.html()).to.contain('<button type="button" name="log-in">Sign in</button>')
+  
+  describe('Logged user', () => {
+    const mocks = {
+      $store: {
+        state: {
+          currentUser: 'sample-user-data'
+        }
+      }
+    }
+
+    it('Renders properly for logged user', () => {
+      const wrapper = mount(Login, {mocks})
+      expect(wrapper.html()).to.contain('You are logged in.')
+    })
   })
 })
