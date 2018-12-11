299c61d62890a2215c8a956e7b6cc5ad164143c6
Piotr Jablonski <piotr.jablonski@snow.dog>
2018-12-11 23:19:13 +0100

    clean firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  postsCollection: ''
+}
+
+export default firebaseConfig
