e33ac4be06cd2b23a0f835dfd1a8f46c5ab3deed
Piotr Jablonski <piotr.jablonski@snow.dog>
2019-03-22 15:24:04 +0100

    Improve time mixin

--- src/mixins/time.js
+++ src/mixins/time.js
@@ -1,8 +1,14 @@
 const time = {
   methods: {
     time() {
-      const date = new Date()
-      return `${date.getDate()}/${(date.getMonth()+1)}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
+      const date = new Date(),
+        day = date.getDate() >= 10 ? date.getDate() : `0${date.getDate()}`,
+        month = date.getMonth() + 1 >= 10 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`,
+        year = date.getFullYear(),
+        hours = date.getHours() >= 10 ? date.getHours() : `0${date.getHours()}`,
+        minutes = date.getMinutes() >= 10 ? date.getMinutes() : `0${date.getMinutes()}`
+
+      return `${day}/${month}/${year} ${hours}:${minutes}`
     }
   }
 }
