7f33e3dd42a857329c24de22e77434e5225c2fb7
Piotr Jablonski <piotr.jablonski@snow.dog>
2019-05-09 00:15:50 +0200

    change dev tools config, giignore, delete unneded logo

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   rules: {
     "indent": ["error", 2],
     "semi": ["error","never"],
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "vue/no-v-html": false
   },
