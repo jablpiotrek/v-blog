7713fb8959cf6624946f099cfbdd64b0af5c50dd
Piotr Jablonski <piotr.jablonski@snow.dog>
2019-05-21 00:47:13 +0200

    import particles background

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app">
     <main-content />
+    <particles-background v-if="particlesBackgroundVisible" />
   </div>
 </template>
 
@@ -11,19 +12,26 @@ import Loading from './components/Loading.vue'
 const MainContent = () => ({
   component: import('./components/MainContent.vue'),
   loading: Loading,
-  delay: 20
+  delay: 50
 })
+
+const ParticlesBackground = () => import('./components/ParticlesBackground')
 
 export default {
   name: "App",
   components: {
-    MainContent
+    MainContent,
+    ParticlesBackground
+  },
+  computed: {
+    particlesBackgroundVisible() {
+      return this.$route.path === "/"
+    }
   }
 }
 </script>
 
 <style lang="scss">
 .app {
-  height: 100vh;
 }
 </style>
