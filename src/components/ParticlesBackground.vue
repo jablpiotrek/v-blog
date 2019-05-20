882b2a747c3fca486c34c233483be8e855365272
Piotr Jablonski <piotr.jablonski@snow.dog>
2019-05-21 00:44:59 +0200

    add particles component

---
+++ src/components/ParticlesBackground.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    id="particles-backround"
+    class="particles-backround"
+  />
+</template>
+
+<script>
+import 'particles.js'
+import particlesConfig from '../assets/config/particles.js'
+
+export default {
+  name: 'ParticlesBackground',
+  mounted() {
+    window.particlesJS('particles-backround', particlesConfig)
+  }
+}
+</script>
+
+<style>
+.particles-backround {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+}
+</style>
