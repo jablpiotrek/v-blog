8086bfd8f1570ca3d6ede8d981403410c6d095f3
Piotr Jablonski <piotr.jablonski@snow.dog>
2019-05-21 00:44:45 +0200

    add particles config

---
+++ src/assets/config/particles.js
@@ -0,0 +1,101 @@
+export default {
+  "particles": {
+    "number": {
+      "value": 180,
+      "density": {
+        "enable": false,
+        "value_area": 2000
+      }
+    },
+    "color": {
+      "value": "#990a20"
+    },
+    "shape": {
+      "type": "circle",
+      "polygon": {
+        "nb_sides": 1
+      }
+    },
+    "opacity": {
+      "value": 0.3,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 1,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 100,
+      "color": "#990a20",
+      "opacity": 0.2,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "window",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "resize": false
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 0.5
+        }
+      },
+      "bubble": {
+        "distance": 100,
+        "size": 1.5,
+        "duration": 2,
+        "opacity": 1,
+        "speed": 1
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.2
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
