e0d43ff63feb1e15efac68dce64ab77a6f2d96d8
Piotr Jablonski <piotr.jablonski@snow.dog>
2019-05-23 13:55:23 +0200

    Update background

--- src/assets/config/particles.js
+++ src/assets/config/particles.js
@@ -1,10 +1,10 @@
 export default {
   "particles": {
     "number": {
-      "value": 220,
+      "value": 150,
       "density": {
-        "enable": false,
-        "value_area": 2000
+        "enable": true,
+        "value_area": 1000
       }
     },
     "color": {
