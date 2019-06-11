4c28cfdcf663347b92a4d9a63916fda6ce5c1fcd
Piotr Jablonski <piotr.jablonski@snow.dog>
2019-06-12 00:35:23 +0200

    Update footer, add icon component

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,16 +1,58 @@
 <template>
-  <footer>
-    <a href="">LinkedIn</a>
-    <a href="">GitHub</a>
+  <footer class="foot">
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://www.linkedin.com/in/piotr-jablonski-008b2799/"
+      class="foot__item"
+    >
+      <icon
+        title="linkedin"
+        class-name="foot__icon"
+      />
+    </a>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://github.com/jablpiotrek"
+      class="foot__item"
+    >
+      <icon
+        title="github"
+        class-name="foot__icon"
+      />
+    </a>
   </footer>
 </template>
 
 <script>
+import Icon from './Icon.vue'
 export default {
-  name: 'Foot'
+  name: 'Foot',
+  components: {
+    Icon
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.foot {
+  display: flex;
+  justify-content: center;
+  background-color: $primary;
+  border-top: 12px solid $secondary;
 
+  &__item {
+    display: block;
+    margin: 0 $spacer-small;
+  }
+
+  &__icon {
+    fill: $white;
+    &:hover,
+    &:focus {
+      fill: $secondary;
+    }
+  }
+}
 </style>
---
+++ src/components/Icon.vue
@@ -0,0 +1,48 @@
+<template>
+  <i
+    :data-feather="title"
+  />
+</template>
+
+<script>
+import feather from 'feather-icons'
+export default {
+  name: 'Icon',
+  props: {
+    title: {
+      required: true,
+      type: String
+    },
+    className: {
+      required: false,
+      type: String,
+      default: ''
+    },
+    size: {
+      required: false,
+      type: Number,
+      default: 36
+    }
+  },
+  mounted() {
+    feather.replace({
+      class: `icon ${this.className}`,
+      height: this.size,
+      width: this.size
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  padding: 6px;
+  fill: $secondary;
+  @include transition(fill);
+
+  &:hover,
+  &:focus {
+    fill: $primary;
+  }
+}
+</style>
